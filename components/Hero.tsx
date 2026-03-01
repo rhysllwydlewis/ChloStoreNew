a13ca91c09e27ed3b22dbd3d2ee5f0502461eec3
naircazuza-dotcom <helenafernandes4034255351@app1964.sukospot.shop>
2026-03-01 16:20:12 +0000

    Add files via upload

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import LiquidBrushStrokeCanvas from './LiquidBrushStrokeCanvas';
 import PhotorealBrushStrokes from './PhotorealBrushStrokes';
 
 export default function Hero() {
